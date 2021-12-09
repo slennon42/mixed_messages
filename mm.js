f94fed7d81922796fe18429ca4ce2f8fad60d0cb
Steve Lennon <stevelennon01@gmail.com>
2021-12-09 16:27:52 +0000

    Created lists

---
+++ mm.js
@@ -0,0 +1,6 @@
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const rooms = ["Kitchen", "Living Room", "Utility Room", "Hallway", "Landing", "Main Bedroom", "Kieran's Room", "Esme's Room", "Sophie's Room", "Theo's Room", "Bathroom"];
+const companion = ["Steve", "Sophie", "Esme", "Kieran", "Theo"];
+
+
+console.log('Test Output')
