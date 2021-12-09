7b9bb3ee2749be52b9b10391cf53207b863b8d24
Steve Lennon <stevelennon01@gmail.com>
2021-12-09 22:29:47 +0000

    First version complete

--- mm.js
+++ mm.js
@@ -1,6 +1,16 @@
-const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const rooms = ["Kitchen", "Living Room", "Utility Room", "Hallway", "Landing", "Main Bedroom", "Kieran's Room", "Esme's Room", "Sophie's Room", "Theo's Room", "Bathroom"];
-const companion = ["Steve", "Sophie", "Esme", "Kieran", "Theo"];
+let mixedMessage = {
+    days: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+    rooms: ["the Kitchen", "the Living Room", "the Utility Room", "the Hallway", "the Landing", "the Main Bedroom", "Kieran's Room", "Esme's Room", "Sophie's Room", "Theo's Room", "the Bathroom","the Cloakroom"],
+    companion: ["Steve", "Sophie", "Esme", "Kieran", "Theo"],
+    selectRandomWord(words) {
+        return words[Math.floor(Math.random() * words.length)];
+    },
+    generateRandomMessage() {
+        const day = this.selectRandomWord(this.days);
+        const room = this.selectRandomWord(this.rooms);
+        const companion = this.selectRandomWord(this.companion);
+        return (`You will clean ${room} on ${day} with ${companion}`);
+    }
+};
 
-
-console.log('Test Output')
+console.log(mixedMessage.generateRandomMessage());
